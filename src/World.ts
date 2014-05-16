7451586f093860be8e35601e11d370c7c0789d29
Dimitri Larue <siderax@gmail.com>
2014-05-16 18:12:25 +0200

    Add separate loop between requestanimation on frame (for render) and logic

--- src/World.ts
+++ src/World.ts
@@ -1,26 +1,27 @@
 module Sef {
 
     export class World {
-        public systems: System[];
-        public entities: Entity[];
+        public systems: System[] = [];
+        public systemsAnimFrame: System[] = [];
+        public entities: Entity[] = [];
 
-        public time: number;
+        public time: number = Date.now();
         public delta: number;
 
-        constructor() {
-            this.time = Date.now();
-
-            this.systems = [];
-            this.entities = [];
-        }
+        constructor(public interval: number = 50) {}
 
         /**
          * Register a system
          *
          * @param {system} System
          */
-        public setSystem(system: System): void {
-            this.systems.push(system);
+        public setSystem(system: System, requestAnimFrame: boolean = true): void {
+            if (requestAnimFrame) {
+                this.systemsAnimFrame.push(system);
+            }
+            else {
+                this.systems.push(system);
+            }
 
             system.world = this;
             system.init();
@@ -41,10 +42,16 @@ module Sef {
          * [refresh description]
          */
         public refresh(e: Entity): void {
-            var systems = this.systems;
+            var l = this.systems.length;
+            var i;
 
-            for (var i = 0, max = systems.length; i < max; i++){
-                systems[i].refreshEntity(e);
+            for (i = 0; i < l; i++){
+                this.systems[i].refreshEntity(e);
+            }
+
+            l = this.systemsAnimFrame.length;
+            for (i = 0; i < l; i++){
+                this.systemsAnimFrame[i].refreshEntity(e);
             }
         }
 
@@ -55,13 +62,36 @@ module Sef {
             var now = Date.now();
             this.delta = now - this.time;
             this.time = now;
-
+            
             var systems = this.systems;
 
             for (var i = 0, max = systems.length; i < max; i++){
                 systems[i].process();
             }
         }
+
+        public processAnimFrame(): void {
+            var systems = this.systemsAnimFrame;
+
+            for (var i = 0, max = systems.length; i < max; i++){
+                systems[i].process();
+            }
+        }
+
+        public start(): void {
+            var that = this;
+
+            function processAnimFrame() {
+                requestAnimationFrame(processAnimFrame);
+                that.processAnimFrame();
+            }
+            processAnimFrame();
+
+            function process() {
+                setTimeout(process, that.interval);
+                that.process();
+            }
+            process();
+        }
     }
 }
-
