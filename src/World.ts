091ee94f8255bee6b7031c1aebd62237c5a995d2
Dimitri Larue <siderax@gmail.com>
2014-05-27 17:57:25 +0200

    Add semi-fixed timestep

--- src/World.ts
+++ src/World.ts
@@ -1,14 +1,19 @@
 module Sef {
 
     export class World {
-        public systems: System[] = [];
+        public systemsSetTimeout: System[] = [];
         public systemsAnimFrame: System[] = [];
+        public renderingSystem: System;
+
         public entities: Entity[] = [];
 
-        public time: number = Date.now();
+        public timeSetTimeout: number = Date.now();
         public timeAnimFrame: number = Date.now();
 
-        constructor(public interval: number = 50) {}
+        public fixedStep: number = 1 / 60 * 1000; //60Hz;
+        public maxInterval: number = 1 / 20 * 1000; //20hz;
+
+        constructor() {}
 
         /**
          * Register a system
@@ -20,8 +25,15 @@ module Sef {
                 this.systemsAnimFrame.push(system);
             }
             else {
-                this.systems.push(system);
+                this.systemsSetTimeout.push(system);
             }
+
+            system.world = this;
+            system.init();
+        }
+
+        public setRenderingSystem(system: System): void {
+            this.renderingSystem = system;
 
             system.world = this;
             system.init();
@@ -42,60 +54,85 @@ module Sef {
          * [refresh description]
          */
         public refresh(e: Entity): void {
-            var l = this.systems.length;
+            var l = this.systemsSetTimeout.length;
             var i;
 
             for (i = 0; i < l; i++){
-                this.systems[i].refreshEntity(e);
+                this.systemsSetTimeout[i].refreshEntity(e);
             }
 
             l = this.systemsAnimFrame.length;
             for (i = 0; i < l; i++){
                 this.systemsAnimFrame[i].refreshEntity(e);
             }
+
+            if (this.renderingSystem) {
+                this.renderingSystem.refreshEntity(e);
+            }
         }
 
         /**
          * [refresh description]
          */
-        public process(): void {
+        public processSetTimeout(): void {
             var now = Date.now();
-            var delta = now - this.time;
-            this.time = now;
+            var delta = now - this.timeSetTimeout;
+            delta = Math.min(delta, this.maxInterval);
 
-            var systems = this.systems;
+            this.timeSetTimeout = now;
 
-            for (var i = 0, max = systems.length; i < max; i++){
-                systems[i].process(this.time, delta);
-            }
+            var systems = this.systemsSetTimeout;
+
+            this.logicWorldLoop(systems, this.timeSetTimeout, delta);
         }
 
         public processAnimFrame(): void {
             var now = Date.now();
             var delta = now - this.timeAnimFrame;
+            delta = Math.min(delta, this.maxInterval);
+
             this.timeAnimFrame = now;
 
             var systems = this.systemsAnimFrame;
 
-            for (var i = 0, max = systems.length; i < max; i++){
-                systems[i].process(this.timeAnimFrame, delta);
+            this.logicWorldLoop(systems, this.timeAnimFrame, delta);
+            this.renderingSystem.process(this.timeAnimFrame, delta);
+
+        }
+
+        public logicWorldLoop(systems: System[], time: number, delta: number): void {
+            if (systems.length === 0) {
+                return;
+            }
+
+            var deltaStep: number = 0;
+            while (delta > 0.0) {
+                deltaStep = Math.min(delta, this.fixedStep);
+                for (var i = 0, max = systems.length; i < max; i++){
+                    systems[i].process(time, deltaStep);
+                }
+
+                delta -= deltaStep;
             }
         }
 
         public start(): void {
-            var that = this;
 
-            function processAnimFrame() {
+            if (typeof this.renderingSystem === 'undefined') {
+                throw new Error('World.renderingSystem is not set.');
+            }
+
+            var processAnimFrame = () => {
                 requestAnimationFrame(processAnimFrame);
-                that.processAnimFrame();
+                this.processAnimFrame();
             }
             processAnimFrame();
 
-            function process() {
-                setTimeout(process, that.interval);
-                that.process();
+            var processSetTimeout = () => {
+                setTimeout(processSetTimeout, this.fixedStep);
+                this.processSetTimeout();
             }
-            process();
+            processSetTimeout();
         }
     }
 }
